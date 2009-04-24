2969118c825c73f1e8bed860918351a6dd670037
Matthew Brennan Jones <mattjones@workhorsy.org>
2009-04-23 21:30:40 -0700

    Fixed bug that had note guids as not required or shown by the tomboy_note model.

--- server/app/models/tomboy_note.rb
+++ server/app/models/tomboy_note.rb
@@ -3,5 +3,5 @@
 class TomboyNote < ActiveRecord::Base
 	belongs_to :user
 
-	validates_presence_of :user_id, :name, :body, :message => 'is required'
+	validates_presence_of :user_id, :name, :body, :guid, :message => 'is required'
 end

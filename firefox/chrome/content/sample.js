e549ec5f51a74ddd5adadaa98cbb9d8c854d0ea8
Matthew Brennan Jones <mattjones@workhorsy.org>
2009-11-16 18:01:05 -0800

    Added basic firefox bookmark events to the plugin.

--- firefox/chrome/content/sample.js
+++ firefox/chrome/content/sample.js
@@ -6,74 +6,94 @@ https://developer.mozilla.org/en/Building_an_Extension
 When developing, link to the extensions dir
 ln -s ~/Desktop/sample/ ~/.mozilla/firefox/uryzgmqw.dev/extensions/spacepony@workhorsy.org
 */
-var dbusnotify = {
-	onLoad: function() {
-		this.initialized = true;
-		this.strings = document.getElementById("dbusnotify-strings");
-	
-		this.dlMgr = Components.classes["@mozilla.org/download-manager;1"]
-						.getService(Components.interfaces.nsIDownloadManager);
-		this.dlMgr.addListener(dbusnotify);
 
-		// Start the dbus server
+// Bookmark Syncer
+// https://developer.mozilla.org/en/Code_snippets/Bookmarks
+var BookmarkManager = {
+	onBeginUpdateBatch: function() {},
+	onEndUpdateBatch: function() {},
+	onItemAdded: function(aItemId, aFolder, aIndex) { alert("onItemAdded"); },
+	onItemRemoved: function(aItemId, aFolder, aIndex) { alert("onItemRemoved"); },
+	onItemChanged: function(aBookmarkId, aProperty, aIsAnnotationProperty, aValue) { alert("onItemChanged"); },
+	onItemVisited: function(aBookmarkId, aVisitID, time) {},
+	onItemMoved: function(aItemId, aOldParent, aOldIndex, aNewParent, aNewIndex) {},
+	QueryInterface: XPCOMUtils.generateQI([Components.interfaces.nsINavBookmarkObserver]), 
+
+	StartListening: function() {
+		var bmsvc = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"]
+				   .getService(Components.interfaces.nsINavBookmarksService);
+		bmsvc.addObserver(BookmarkManager, false);
+	}, 
+
+	StoptListening: function() {
+		var bmsvc = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"]
+				   .getService(Components.interfaces.nsINavBookmarksService);
+		bmsvc.addObserver(BookmarkManager, false);
+	}, 
+
+	InitialSync: function() {
+		//alert("InitialSync");
+		var bmsvc = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"]
+		                    .getService(Components.interfaces.nsINavBookmarksService);
+		var ios = Components.classes["@mozilla.org/network/io-service;1"]
+		                    .getService(Components.interfaces.nsIIOService);
+		var menuFolder = bmsvc.bookmarksMenuFolder; // Bookmarks menu folder
+		var newFolderId = bmsvc.createFolder(menuFolder, "pies of mud", bmsvc.DEFAULT_INDEX);
+		var uri = ios.newURI("http://google.com/", null, null);
+		var newBkmkId = bmsvc.insertBookmark(newFolderId, uri, bmsvc.DEFAULT_INDEX, "google");
+	}, 
+};
+
+var Helper = {
+	RunProcess: function(is_blocking, process_path, args) {
 		try {
-			// Get the path of the dbus script
+			// Get the path of the script
 			const DIR_SERVICE = new Components.Constructor("@mozilla.org/file/directory_service;1", "nsIProperties");
 			var path = (new DIR_SERVICE()).get("ProfD", Components.interfaces.nsIFile).path;
-			path += "/extensions/spacepony@workhorsy.org/chrome/content/dbusnotify_server.py";
+			path += process_path;
 
 			// https://developer.mozilla.org/En/Code_snippets/Running_applications
 			// create an nsILocalFile for the executable
 			var file = Components.classes["@mozilla.org/file/local;1"]
-						.createInstance(Components.interfaces.nsILocalFile);
+			            .createInstance(Components.interfaces.nsILocalFile);
 			file.initWithPath(path);
 
 			// If it failed, show an error
 			if(!file.exists()) {
-				alert("Error running dbus server");
-				return;
+				throw("File to run not found: " + process_path);
 			}
 
 			// Create and run the process
 			var process = Components.classes["@mozilla.org/process/util;1"]
-							.createInstance(Components.interfaces.nsIProcess);
-			var args = [];
+			                .createInstance(Components.interfaces.nsIProcess);
 			process.init(file);
-			process.run(false, args, args.length);
+			process.run(is_blocking, args, args.length);
 		} catch(exception) {
-			alert("dbus server call Failed" + exception);
-			return;
+			throw("Running process failed: " + String(exception));
 		}
+	}
+}
+
+var DownloadManager = {
+	StartListening: function() {
+		// Start the dbus server in a background process
+		Helper.RunProcess(
+		false, 
+		"/extensions/spacepony@workhorsy.org/chrome/content/dbusnotify_server.py", 
+		[]);
+
+		// Tell the download manager to send events to the DownloadManager
+		this.dlMgr = Components.classes["@mozilla.org/download-manager;1"]
+						.getService(Components.interfaces.nsIDownloadManager);
+		this.dlMgr.addListener(DownloadManager);
 	},
 
-	notify: function(aDownload) {
-		try {
-			// Get the path of the dbus script
-			const DIR_SERVICE = new Components.Constructor("@mozilla.org/file/directory_service;1", "nsIProperties");
-			var path = (new DIR_SERVICE()).get("ProfD", Components.interfaces.nsIFile).path;
-			path += "/extensions/spacepony@workhorsy.org/chrome/content/dbusnotify_fire_download_complete.py";
-
-			// create an nsILocalFile for the executable
-			var file = Components.classes["@mozilla.org/file/local;1"]
-						.createInstance(Components.interfaces.nsILocalFile);
-			file.initWithPath(path);
-
-			// If it failed, show an error
-			if(!file.exists()) {
-				alert("Error running dbusnotify_fire_download_complete.py");
-				return;
-			}
-
-			// Create and run the process
-			var process = Components.classes["@mozilla.org/process/util;1"]
-							.createInstance(Components.interfaces.nsIProcess);
-			var args = ["Download Complete", aDownload.targetFile.path];
-			process.init(file);
-			process.run(true, args, args.length);
-		} catch(exception) {
-			alert("DBus Notification Failed" + exception);
-			return;
-		}
+	SendNotification: function(aDownload) {
+		// Send a dbus event to the server
+		Helper.RunProcess(
+		true, 
+		"/extensions/spacepony@workhorsy.org/chrome/content/dbusnotify_fire_download_complete.py", 
+		["Download Complete", aDownload.targetFile.path]);
 	},
 
 	onDownloadStateChange: function(aState, aDownload) {
@@ -83,12 +103,25 @@ var dbusnotify = {
 			case Components.interfaces.nsIDownloadManager.DOWNLOAD_CANCELED:
 				break;
 			case Components.interfaces.nsIDownloadManager.DOWNLOAD_FINISHED:
-				this.notify(aDownload);
+				this.SendNotification(aDownload);
 				break;
 		}
 	},
 
 };
 
-window.addEventListener("load", function(e) { dbusnotify.onLoad(e); }, false);
+var Program = {
+	onLoad: function() {
+		this.initialized = true;
+		//this.strings = document.getElementById("dbusnotify-strings");
+		// Start listening to downloads
+		DownloadManager.StartListening();
+
+		// Sync the bookmarks, and continue listening for changes
+		BookmarkManager.InitialSync();
+		BookmarkManager.StartListening();
+	}, 
+}
+
+window.addEventListener("load", function(e) { Program.onLoad(e); }, false);
 
